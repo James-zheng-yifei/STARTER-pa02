4358ce1c337aedbe915f57b09ec9c9e72cf740a4
Yifei Zheng <yifeizheng@cs.ucsb.edu>
2026-02-24 16:16:45 -0800

    000

--- movies.cpp
+++ movies.cpp
@@ -22,7 +22,7 @@ void Movies::printMoviesInOrder() const {
     }
 }
 
-pair<vector<pair<string,double>>::const_iterator,
+/*pair<vector<pair<string,double>>::const_iterator,
      vector<pair<string,double>>::const_iterator>
 Movies::getPrefixRange(const string& prefix) const {
 
@@ -42,6 +42,26 @@ Movies::getPrefixRange(const string& prefix) const {
     while(end != movies.end() && end->first.starts_with(prefix)) {
         ++end;
     }
+
+    return {start, end};
+}*/
+pair<vector<pair<string,double>>::const_iterator,
+     vector<pair<string,double>>::const_iterator>
+Movies::getPrefixRange(const string& prefix) const {
+
+	auto start = lower_bound(movies.begin(), movies.end(), prefix,
+		[](const auto& a,const string& pref){
+			return a.first<pref;
+		});
+    
+    if (start == movies.end() || !start->first.starts_with(prefix)) {
+        return {movies.end(), movies.end()};
+    }
+
+    auto end = std::upper_bound(start, movies.end(), prefix,
+        [](const string& pref, const auto& a) {
+            return !a.first.starts_with(pref);
+        });
 
     return {start, end};
 }
